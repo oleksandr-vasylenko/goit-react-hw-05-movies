b040daad44191ac4b4d6d95194fa9105edc41972
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-01-12 01:41:24 +0200

    Misc updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,37 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import { Home } from '../../pages/Home';
+import { Home } from '../../pages/Home/Home';
 import { Movies } from '../Movies/Movies';
-import { MovieDetails } from '../MovieDetails/MovieDetails';
+import { MovieDetails } from '../../pages/MovieDetails/MovieDetails';
 import { Cast } from '../Cast/Cast';
 import { Reviews } from '../Reviews/Reviews';
+import { getTrending } from '../../utils/api';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+  &.active {
+    color: orange;
+  }
+`;
 
 export const App = () => {
-  const StyledLink = styled(NavLink)`
-    color: black;
-    &.active {
-      color: orange;
-    }
-  `;
+  console.log(getTrending().then(res => console.log(res)));
 
   return (
     <div>
       <nav>
         <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="/about">Movies</StyledLink>
+        <StyledLink to="/Movies">Movies</StyledLink>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/" element={<Home />}>
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
       </Routes>
     </div>
   );
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const KEY = '68f9ac02869bbba2dc968e1b369c4856';
 
-export async function fetchImages(input, page) {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      key: KEY,
-      q: input,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: 12,
-      page: page,
-    },
-  });
+export async function getTrending() {
+  const response = await axios.get(
+    `${BASE_URL}trending/all/day?api_key=${KEY}`
+  );
 
-  return response.data;
+  return response.data.results;
 }
