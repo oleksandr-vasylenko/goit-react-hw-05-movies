c2b79186a6c470ea0524cffe6aad14418fdeaedf
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-01-15 19:03:17 +0200

    Adjusts date and user score

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -32,9 +32,9 @@ function MovieDetails() {
         width={200}
       />
       <h2>
-        {title} ({release_date})
+        {title} ({release_date && release_date.substring(0, 4)})
       </h2>
-      <p>User Score: {vote_average}</p>
+      <p>User Score: {Math.round(vote_average * 10)}%</p>
       <h3>Overview</h3>
       <p>{overview}</p>
       <h3>Genres</h3>
